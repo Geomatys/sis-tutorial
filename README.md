666e0c9b373892c420e928776e5ec939c82d0b8e
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2023-04-21 14:52:51 +0200

    A dummy application with no dependency to geospatial libraries.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tutorial
+
+This repository illustrates how to use GeoAPI, PROJ-JNI and Apache SIS in a project.
+Each Git commit is a step of the tutorial.
+The initial step is a project before dependencies are added.
---
+++ src/main/java/mycompany/Main.java
@@ -0,0 +1,14 @@
+/*
+ * This tutorial is in public domain.
+ */
+package mycompany;
+
+/**
+ * An application with no direct dependency to the
+ * "referencing by coordinates" or "metadata" library.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The application main class.");
+    }
+}
