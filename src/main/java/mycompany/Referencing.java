40306a5b4a0af19241e79c3485a72378bd568561
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2023-04-21 16:25:01 +0200

    Demonstrate the projection of points.

--- src/main/java/mycompany/Referencing.java
+++ src/main/java/mycompany/Referencing.java
@@ -6,13 +6,16 @@ package mycompany;
 import mycompany.geospatial.Services;
 
 // Implementation-neutral
+import javax.measure.Unit;
 import org.opengis.metadata.extent.Extent;
 import org.opengis.metadata.extent.GeographicBoundingBox;
 import org.opengis.metadata.extent.GeographicExtent;
 import org.opengis.referencing.crs.CRSAuthorityFactory;
 import org.opengis.referencing.crs.GeographicCRS;
 import org.opengis.referencing.crs.ProjectedCRS;
+import org.opengis.referencing.cs.CoordinateSystem;
 import org.opengis.referencing.operation.CoordinateOperation;
+import org.opengis.referencing.operation.MathTransform;
 import org.opengis.referencing.operation.TransformException;
 import org.opengis.util.FactoryException;
 
@@ -39,6 +42,45 @@ class Referencing {
         System.out.printf("Coordinate operation: %s%n", op.getName().getCode());
         System.out.printf("Scope: %s%n", op.getScope());
         printGeographicExtent(op.getDomainOfValidity());
+        System.out.println();
+        /*
+         * Get the axis units of measurement. The unit types are
+         * usually `Unit<Length>` or `Unit<Angle>`, but for this
+         * test we do not need to know the exact type.
+         */
+        final int numDimensions = 2;
+        CoordinateSystem cs = targetCRS.getCoordinateSystem();
+        assert cs.getDimension() == numDimensions : "This tutorial assumes 2D systems.";
+        Unit<?> xUnit = cs.getAxis(0).getUnit();
+        Unit<?> yUnit = cs.getAxis(1).getUnit();
+        /*
+         * Coordinates are given with only 3 significant digits,
+         * which implies an inaccuracy of more than 100 metres.
+         * With such error, it does not matter if the geodetic
+         * reference frame is WGS84, NAD83 or NAD27.
+         *
+         * Axis order is latitude first, then longitude.
+         * Units are degrees.
+         */
+        double[] coordinates = {
+            49.250, -123.100,           // Vancouver
+            37.783, -122.417,           // San-Francisco
+            45.500,  -73.567,           // Montreal
+            40.713,  -74.006,           // New-York
+            48.801,    2.351,           // Paris
+            35.666,  139.772};          // Tokyo
+        /*
+         * It would be possible to transform point-by-point,
+         * but bulk transforms are much faster.
+         */
+        int numPoints = coordinates.length / numDimensions;
+        MathTransform mt = op.getMathTransform();
+        mt.transform(coordinates, 0, coordinates, 0, numPoints);
+        for (int i=0; i<numPoints; i++) {
+            System.out.printf("City location: %9.0f %s, %8.0f %s%n",
+                    coordinates[i*numDimensions  ], xUnit,
+                    coordinates[i*numDimensions+1], yUnit);
+        }
     }
 
     private static void printGeographicExtent(Extent domain) {
